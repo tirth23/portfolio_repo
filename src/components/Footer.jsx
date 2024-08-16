4938d8f93c9241fe6316224f446b0114e7684e58
Tirth Patel <tirthpatel23t@gmail.com>
2024-08-16 19:48:29 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import { SOCIAL_MEDIA_LINKS } from "../constants";
+import { motion } from "framer-motion";
+
+const Footer = () => {
+	return (
+		<div className="mb-8 mt-20">
+			<div className="flex items-center justify-center">
+				<motion.img
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					transition={{ duration: 0.5 }}
+					src={logo}
+					width={200}
+					className="my-20"
+				/>
+			</div>
+			<div className="flex items-center justify-center gap-8">
+				{SOCIAL_MEDIA_LINKS.map((link, index) => (
+					<motion.a
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1 }}
+						transition={{ duration: 0.2, delay: index * 0.5 }}
+						href={link.href}
+						target="_blank"
+						rel="noopener noreferrer"
+						key={index}
+					>
+						{link.icon}
+					</motion.a>
+				))}
+			</div>
+			<p className="mt-8 text-center text-sm tracking-wide text-gray-400">
+				&copy;compileTab. All rights reserved.
+			</p>
+		</div>
+	);
+};
+
+export default Footer;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { HERO } from "../constants";
+import carlImg from "../assets/carl.jpeg";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+	return (
+		<section className="flex min-h-screen flex-wrap items-center">
+			<motion.div
+				initial={{ opacity: 0, x: -50 }}
+				animate={{ opacity: 1, x: 0 }}
+				transition={{ duration: 1 }}
+				className="w-full md:w-1/2"
+			>
+				<h2 className="my-8 p-2 text-4xl font-bold md:text-5xl lg:text-[7rem]">
+					{HERO.name}
+				</h2>
+				<p className="p-2 text-3xl tracking-tightier lg:text-4xl">
+					{HERO.greet}
+				</p>
+				<p className="mb-8 p-2 text-xl">{HERO.description}</p>
+			</motion.div>
+			<motion.div
+				initial={{ opacity: 0, scale: 0.8 }}
+				animate={{ opacity: 1, scale: 1 }}
+				transition={{ duration: 1 }}
+				className="w-full md:w-1/2 lg:p-8"
+			>
+				<div className="flex justify-center">
+					<motion.img
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 1 }}
+						src={carlImg}
+						width={550}
+						height={550}
+						alt="carl-img"
+						className="rounded-3xl"
+					/>
+				</div>
+			</motion.div>
+		</section>
+	);
+};
+
+export default Hero;
